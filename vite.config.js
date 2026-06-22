4cc7a003d787091b820e6d31fd8344eaef46a62a
abrar216 <abrar216@users.noreply.github.com>
2026-06-22 12:17:11 +0500

    fix: restore vercel.json and vite.config.js production configurations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: process.env.NODE_ENV === 'production' ? '/abrar01/public/build/' : '/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
